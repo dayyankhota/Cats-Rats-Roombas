da3060af520d1a03f3124dc8c21651b5cdf3d235
SmilingRae <rsantana06621@gmail.com>
2026-05-03 15:42:44 +0200

    audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header ("--- Audio Source---")]
+    [SerializeField] AudioSource musicSource;
+      [SerializeField] AudioSource SFXSource;
+
+      [Header ("--- Audio Clip---")]
+      public AudioClip background;
+      public AudioClip death;
+      public AudioClip attack;
+      public AudioClip pickup;
+}
